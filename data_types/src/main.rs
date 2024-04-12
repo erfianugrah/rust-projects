52084527c8cc4449b22ffa80a5b3ac15746acd6b
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-04-12 06:10:25 +0000

    data types

---
+++ data_types/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    // floating point types
+    let x = 2.0; // f64
+    let y: f32 = 3.0; //f32
+    let sum = 5 + 10;
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let remainder = 43 % 5;
+    let t = true;
+
+    let f: bool = false; // with explicit type annotation
+    let c = 'z';
+    let z: char = 'ℤ'; // with explicit type annotation
+    let heart_eyed_cat = '😻';
+}
