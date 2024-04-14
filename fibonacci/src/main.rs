434c22c4436694efdc1cf877a1db8723bdf7fdc9
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-04-14 08:26:23 +0000

    fib edits

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -23,9 +23,9 @@ fn main() {
 
 fn fibonacci(n: i32) -> i32 {
     if n <= 0 {
-        return 0;
+        0
     } else if n == 1 {
-        return 1;
+        1
     } else {
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
