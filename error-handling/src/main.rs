171a080e4b56e9e76ce69ef8c022708103e3ccab
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-04-26 17:08:30 +0000

    panik

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    // panic!("lele panik!"); // add panic ='abort' to toml file if you want to make the binary as
+    // small as possible
+    let v = vec![1, 2, 3];
+    v[99]; // in C, you might get whatever is at the location that would correspond to that
+    // element, buffer overread
 }
+
