be4dc500adac7f4b1509413ffbc229b12c2f3bba
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-05-02 14:13:04 +0000

    separation of concerns

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,13 +2,20 @@ use std::env; // to able to use the args library, use args_os if unicode
 use std::fs; // file system library
 fn main() {
     let args: Vec<String> = env::args().collect(); // we call the collect() method on an iterator
-                                                   // to turn it into a collection, such as a vector
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args); // to turn it into a collection, such as a vector
+                                                  // let query = &args[1];
+                                                  // let file_path = &args[2];
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file"); // In main, the new statement fs::read_to_string takes the file_path, opens that file, and returns a std::io::Result<String> of the file’s contents.
     println!("With text:\n{contents}");
     // dbg!(args);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
